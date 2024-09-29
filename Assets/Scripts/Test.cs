eb3770a1d4e881fb36c9cd2e286397b1ad3db937
xuxuxu <xuxuxu521124@163.com>
2024-09-29 10:56:39 +0800

    编辑场景

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        num++;
+        Debug.Log(num);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
